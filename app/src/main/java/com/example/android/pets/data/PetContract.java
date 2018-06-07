66d5d1b0a80a85a93facc1145a7212542127fb51
bruno78 <devbruno78@gmail.com>
2018-06-07 17:12:05 -0400

    Add PetProvier and Uri constants on PetContract

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,6 +1,9 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import java.net.URI;
 
 /**
  * This is a contract class where it's declared as a final because it can't be extended or implement
@@ -8,9 +11,15 @@ import android.provider.BaseColumns;
  */
 public final class PetContract {
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     private PetContract(){};
 
     public static abstract class PetEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public static final String TABLE_NAME = "pets";
 
